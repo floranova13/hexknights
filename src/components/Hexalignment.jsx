39fdeb85563f840a12894bf19f0c634d3e5bb2a6
Daniel C <50636323+floranova13@users.noreply.github.com>
2021-10-15 16:14:48 -0400

    Add icons to Hexalignment component

--- src/components/Hexalignment.jsx
+++ src/components/Hexalignment.jsx
@@ -2,7 +2,16 @@
 import React, { useEffect } from 'react';
 import HexknightCard from './HexknightCard';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlusSquare, faMinusSquare } from '@fortawesome/free-solid-svg-icons';
+import {
+  faPlusSquare,
+  faMinusSquare,
+  faFire,
+  faMoon,
+  faFlask,
+  faEye,
+  faShieldAlt,
+  faBurn,
+} from '@fortawesome/free-solid-svg-icons';
 
 const Hexalignment = ({
   hexalignment,
@@ -10,7 +19,22 @@ const Hexalignment = ({
   expanded,
   toggleExpansion,
 }) => {
-  useEffect(() => {});
+  const getIcon = (hexalignment) => {
+    switch (hexalignment) {
+      case 'Judgement':
+        return faFire;
+      case 'Mercy':
+        return faMoon;
+      case 'Progress':
+        return faFlask;
+      case 'Expedition':
+        return faEye;
+      case 'Resilience':
+        return faShieldAlt;
+      default:
+        return faBurn;
+    }
+  };
 
   const knightList = hexknights.map((knight, i) => {
     return <HexknightCard key={i} hexknight={knight} />;
@@ -28,6 +52,7 @@ const Hexalignment = ({
           inline
         />
         <h1 className='header hexalignment-title'>{`Hexalignment: ${hexalignment}`}</h1>
+        <FontAwesomeIcon size='2x' icon={getIcon(hexalignment)} />
       </div>
       <div id='hexalignment-hexknight-container'>
         {expanded ? knightList : []}
