1ef2a3e5e249c47d01e1d96a600ab82310723db2
Daniel C <50636323+floranova13@users.noreply.github.com>
2021-11-21 16:22:18 -0500

    Add more lore description

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import Text from './Text';
 import info from '../common/resources/generalInformation.json';
 
 const GeneralInfo = () => {
@@ -8,9 +9,9 @@ const GeneralInfo = () => {
       <h1 className='page-title'>Wayward Faction Archive</h1>
       <h1 className='header'>{info.general.missionName}</h1>
       <h1 className='subheader subheader-left'>Mission Statement:</h1>
-      <p className='text'>{info.general.missionStatement}</p>
+      <Text paragraphs={info.general.missionStatement} classes='description' />
       <h1 className='subheader subheader-left'>Mission Description:</h1>
-      <p className='text'>{info.general.missionDescription}</p>
+      <Text paragraphs={info.general.missionDescription} classes='text' />
     </div>
   );
 };
--- src/components/ShroudInfo.jsx
+++ src/components/ShroudInfo.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import Text from './Text';
 import info from '../common/resources/generalInformation.json';
 
 const ShroudInfo = () => {
@@ -7,32 +8,13 @@ const ShroudInfo = () => {
     <div className='shroud-info-container'>
       <h1 className='page-title'>Shroud</h1>
       <h1 className='header header-left'>The Covenant of Shade:</h1>
-      <p className='text'>
-        The only acceptable faith in the Shrouded Theocracy. All others are
-        divined to be the work of the Strangers, and purged with extreme
-        prejudice.
-      </p>
-      <p className='text'>
-        The followers of the Covenant of Shade are called &quot;The Shaden&quot;
-        or &quot;Shaden&quot;. However, within the territory of the Shrouded
-        Theocracy, people are assumed to be either Shaden or targets for
-        &quot;enforcement&quot;.
-      </p>
+      <Text paragraphs={info.religion.general} classes='text' />
       <h1 className='subheader subheader-left'>Channeler:</h1>
-      <p className='text'>
-        The Channeler is the single highest authority in The Shrouded Theocracy.
-      </p>
+      <Text paragraphs={info.religion.channeler.description} classes='text' />
       <h1 className='subheader subheader-left'>Paragons:</h1>
-      <p className='text'>
-        The Paragons the Shrouded Theocracy&apos;s governers. Every Paragon has
-        a specialty, an aspect of Shroud that they a responsible for
-        maintaining. Each one also commands six Hexknights.
-      </p>
+      <Text paragraphs={info.religion.paragonDescription} classes='text' />
       <h1 className='subheader subheader-left'>Hexaline Knights:</h1>
-      <p className='text'>
-        The Hexaline Knights, also known as Hexknights, are the Shrouded
-        Theocracy&apos;s might. Every Hexknight commands a squad of six knights.
-      </p>
+      <Text paragraphs={info.religion.hexknights} classes='text' />
     </div>
   );
 };
