44bf4d815db6d6974ab3b228a24c684991c7fdaa
Daniel C <50636323+floranova13@users.noreply.github.com>
2021-11-24 00:46:29 -0500

    Adjust BlightPage styling

--- src/pages/BlightPage.jsx
+++ src/pages/BlightPage.jsx
@@ -7,26 +7,52 @@ import blight from '../common/resources/blight.json';
 const BlightPage = () => {
   return (
     <Page>
-      <div className='page-container'>
+      <div className='blight-page-container page-container'>
         <h1 className='page-title'>Blight</h1>
         <Text paragraphs={blight.blightIntro} classes='text' />
-        <hr />
         <Text paragraphs={blight.blightDescription} classes='text' />
-        <h1 className='header-left'>Festerfonts</h1>
-        <Text paragraphs={blight.festerfontsIntro} classes='text' />
-        <Link className='blight-button text' to='/festerfonts'>
-          Festerfonts
-        </Link>
-        <h1 className='header-left'>Blightbeasts</h1>
-        <Text paragraphs={blight.blightbeastsIntro} classes='text' />
-        <Link className='blight-button text' to='/blightbeasts'>
-          Blightbeasts
-        </Link>
-        <h1 className='header-left'>Blightsources</h1>
-        <Text paragraphs={blight.blightsourcesIntro} classes='text' />
-        <Link className='blight-button text' to='/blightsources'>
-          Blightsources
-        </Link>
+        <div className='blight-festerfonts-container blight-section-container'>
+          <h1 className='header-left blight-section-title'>Festerfonts</h1>
+          <div className='blight-inner-section-container'>
+            <div className='blight-subsection-container'>
+              <Text paragraphs={blight.festerfontsIntro} classes='text' />
+            </div>
+            <div className='blight-subsection-right-container'>
+              <div className='subsection-image festerfonts-image'></div>
+              <Link className='blight-button text' to='/festerfonts'>
+                Festerfonts
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className='blight-blightbeasts-container blight-section-container'>
+          <h1 className='header-left blight-section-title'>Blightbeasts</h1>
+          <div className='blight-inner-section-container'>
+            <div className='blight-subsection-container'>
+              <Text paragraphs={blight.blightbeastsIntro} classes='text' />
+            </div>
+            <div className='blight-subsection-right-container'>
+              <div className='subsection-image blightbeasts-image'></div>
+              <Link className='blight-button text' to='/blightbeasts'>
+                Blightbeasts
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className='blight-blightsources-container blight-section-container'>
+          <h1 className='header-left blight-section-title'>Blightsources</h1>
+          <div className='blight-inner-section-container'>
+            <div className='blight-subsection-container'>
+              <Text paragraphs={blight.blightsourcesIntro} classes='text' />
+            </div>
+            <div className='blight-subsection-right-container'>
+              <div className='subsection-image blightsources-image'></div>
+              <Link className='blight-button text' to='/blightsources'>
+                Blightsources
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </Page>
   );
